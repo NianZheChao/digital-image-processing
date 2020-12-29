4e6d75eb89fdc0ef0d76dc6af92f77d7f65c6e69
Nian <nianzhechao@2017.cqut.edu.cn>
2020-12-29 11:02:36 +0800

    daily

--- Functions/finished/txfg2.m
+++ Functions/finished/txfg2.m
@@ -1,19 +1,39 @@
-%Ostu
-I=imread('lenna.jpg');
+ clear;
+I=imread('1.jpg');
+subplot(121),imshow(uint8(I)),title('原图');
 I=rgb2gray(I);
 I=double(I);
-subplot(2,1,1),imshow(uint8(I)),title('原图');
 [m,n]=size(I);
-Th=Otsu(I);
+Smax=0;
+for T=0:255               
+    sum1=0; num1=0;                   
+    sum2=0; num2=0;                   
+    for i=1:m
+        for j=1:n
+            if I(i,j)>=T
+                sum2=sum2+I(i,j); 
+                num2=num2+1;              
+            else 
+                sum1=sum1+I(i,j); 
+                num1=num1+1;                
+            end 
+        end 
+    end  
+    ave1=sum1/num1; 
+    ave2=sum2/num2;
+    S=((ave2-T)*(T-ave1))/(ave2-ave1)^2;
+    if(S>Smax)
+        Smax=S;
+        Th=T;
+     end
+end
 for i=1:m
     for j=1:n
         if I(i,j)>=Th
             I(i,j)=255;
         else
             I(i,j)=0;
-       end
+        end
     end
 end
-subplot(2,1,2),imshow(I),title('Otsu法');   
-
- 
+subplot(122),imshow(I),title('类间最大距离法分割结果');  
